21e8dba4cf8f3d175218cccb5713e9d6b27148a4
Leo Gaskin <leo.gaskin@le0.gs>
2025-06-30 17:29:38 +0200

    Add FAME3R integration code
    This should be moved into the FAME3R repo later

---
+++ gloryxr/should_be_in_fame3r.py
@@ -0,0 +1,78 @@
+# pyright: basic
+
+from typing import Any
+
+import numpy as np
+import numpy.typing as npt
+from CDPL.Chem import AtomProperty, parseSMILES  # type:ignore
+from fame3r.compute_descriptors import DescriptorGenerator, MoleculeProcessor
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.utils._set_output import _SetOutputMixin
+from sklearn.utils.estimator_checks import check_estimator
+from sklearn.utils.validation import check_array, check_is_fitted
+
+
+class Fame3RVectorizer(BaseEstimator, TransformerMixin, _SetOutputMixin):
+    def __init__(self, radius: int = 5) -> None:
+        self.radius: int = radius
+
+    def fit(self, X: Any = None, y: None = None):
+        example_mol = parseSMILES("C")
+        MoleculeProcessor.perceive_mol(example_mol)
+
+        self.inner_ = DescriptorGenerator(radius=self.radius)
+        self.feature_names_ = self.inner_.generate_descriptors(
+            example_mol.getAtom(0), example_mol
+        )[0]
+
+        return self
+
+    def transform(self, X: Any):
+        check_is_fitted(self)
+        check_array(X, dtype="str", estimator=Fame3RVectorizer)
+
+        return np.apply_along_axis(lambda row: self.transform_one(row[0]), 1, X)
+
+    def transform_one(self, X: Any) -> npt.NDArray[np.float64]:
+        check_is_fitted(self)
+
+        if not isinstance(X, str):
+            return self._empty_value()
+
+        cdpkit_marked_educt = parseSMILES(X)
+        MoleculeProcessor.perceive_mol(cdpkit_marked_educt)
+
+        som_atoms_unordered = {
+            atom.getProperty(AtomProperty.ATOM_MAPPING_ID): atom
+            for atom in cdpkit_marked_educt.getAtoms()
+            if atom.getProperty(AtomProperty.ATOM_MAPPING_ID)
+        }
+
+        # TODO: Add warning or error
+        if len(som_atoms_unordered) != 1:
+            return self._empty_value()
+
+        descriptors = [
+            self.inner_.generate_descriptors(
+                som_atoms_unordered[i], cdpkit_marked_educt
+            )[1]
+            for i in sorted(som_atoms_unordered.keys())
+        ]
+
+        return np.asarray(descriptors, dtype=np.float64)[0]
+
+    def get_feature_names_out(self, input_features=None):
+        check_is_fitted(self)
+
+        return self.feature_names_
+
+    def _empty_value(self):
+        check_is_fitted(self)
+
+        return np.full(len(self.feature_names_), np.nan)
+
+
+class ModelMultiplexer(BaseEstimator): ...
+
+
+# check_estimator(Fame3RVectorizer())
