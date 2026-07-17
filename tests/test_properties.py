46426bcfe42b462ab634cdb008e74e056498e3e7
Leo Gaskin <leo.gaskin@le0.gs>
2026-07-17 13:20:39 +0200

    Commit simple test

---
+++ tests/test_properties.py
@@ -0,0 +1,15 @@
+from rdkit.Chem import AllChem as Chem  # type:ignore
+from syrupy.assertion import SnapshotAssertion
+
+from gloryxr.prediction import GLORYxR
+
+PARACETAMOL = Chem.MolFromSmiles("CC(=O)Nc1ccc(O)cc1")
+
+
+def test_predictions_as_expected(snapshot: SnapshotAssertion):
+    gloryxr = GLORYxR(phase="1+2")
+    predictions = gloryxr.predict_one(PARACETAMOL)
+
+    assert [
+        (Chem.ReactionToSmarts(p.concrete_reaction), p.score) for p in predictions
+    ] == snapshot
