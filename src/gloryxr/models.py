b61f8a4288601949320a379ebce543fb9c0ad655
Leo Gaskin <leo.gaskin@le0.gs>
2026-07-23 12:23:47 +0200

    Add initial support for AweSOM models
    The next commits will split gloryxr.models into submodules, as to
    avoid the import tax of torch_geometric and allow for optional dependencies.

--- src/gloryxr/models.py
+++ src/gloryxr/models.py
@@ -5,10 +5,13 @@ from typing import Any, Literal, override
 
 import joblib
 import numpy as np
+from awesom.dataset import mol_to_data
+from awesom.model import SOMPredictor, predict_ensemble
 from fame3r import FAME3RVectorizer
 from rdkit.Chem.rdchem import Mol
 from rdkit.Chem.rdChemReactions import ChemicalReaction
 from rdkit.Chem.rdmolfiles import MolToSmiles
+from torch_geometric.loader import DataLoader
 
 from gloryxr.utils import extract_smiles_for_soms
 
@@ -73,5 +76,54 @@ class MultiFAME3RModelProvider(ModelProvider):
             predictions = self.models[subset].predict_proba(descriptors)
 
             results.append(predictions[:, -1].max() * priority_factor)
+
+        return np.asarray(results)
+
+
+class AweSOMModelProvider(ModelProvider):
+    def __init__(self, model_path: PathLike[str] | str) -> None:
+        self.models = []
+
+        for model_path in sorted(Path(model_path).glob("model_*")):
+            checkpoint_path = model_path / "checkpoints" / "best_model.ckpt"
+            self.models.append(SOMPredictor.load(str(checkpoint_path)))
+
+    @override
+    def predict_proba(
+        self,
+        reactions: list[ChemicalReaction],
+    ) -> np.ndarray[tuple[int], np.dtype[np.float64]]:
+        mol_data = [
+            mol_to_data(rxn.GetReactantTemplate(0), soms=[], mol_id=mol_id, description="")
+            for mol_id, rxn in enumerate(reactions)
+        ]
+
+        # NOTE: this will predict scores for all atoms, and then
+        # filter. Only predicting atoms marked as SOM by the reaction
+        # mechanism would be more efficient.
+        predictions = predict_ensemble(DataLoader(mol_data), self.models)
+
+        probabilities_mapped = {
+            (mol_id, atom_id): prob
+            for mol_id, atom_id, prob in zip(
+                predictions.mol_ids.tolist(),
+                predictions.atom_ids.tolist(),
+                predictions.get_probabilities().mean(dim=0).tolist(),
+                strict=True,
+            )
+        }
+
+        results = []
+        for mol_id, rxn in enumerate(reactions):
+            results.append(
+                max(
+                    [
+                        probabilities_mapped[(mol_id, atom.GetIdx())]
+                        for atom in rxn.GetReactantTemplate(0).GetAtoms()
+                        if atom.GetAtomMapNum() != 0
+                    ],
+                    default=np.nan,
+                )
+            )
 
         return np.asarray(results)
